2b0f045e3b6c33216c18a051832d0bbc765b750a
kishan <Kash.hj94@gmail.com>
2020-05-29 19:21:39 -0400

    Added datasource

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from 'react';
-import Card from './components/Card';
+import React, { useState, useEffect } from 'react';
+import Header from "./components/Header"
+import Axios from 'axios';
 
 function App() {
+  
+  const [profileData,setProfileData] = useState(undefined);
+
+    useEffect(() => {
+      async function getData(){
+        try {
+          
+          // const {data} = await Axios.get("https://us-central1-testingfunctions-6f247.cloudfunctions.net/testing");
+          // setProfileData(data);
+          const {localData} = require("./data/data.json")
+          setProfileData(localData);
+          
+        } catch (error) {
+          console.log(error);
+          const {localData} = require("./data/data.json")
+          setProfileData(localData);
+        }
+      };
+      getData();
+    },[]);
+
+
+  if(!profileData)
+  return ( <div className='wrapper'> Loading ... </div>);
+
   return (
-    <Card className="App">
-      <div>
-        Test
-      </div>
-    </Card>
+    <div className='wrapper'>
+      <Header />
+    </div>
   );
 }
 
